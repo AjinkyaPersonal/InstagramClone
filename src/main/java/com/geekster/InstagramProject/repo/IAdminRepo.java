609cc18e13a877a09d95ef7908a77c4a36319825
AjinkyaPersonal <106467389+AjinkyaPersonal@users.noreply.github.com>
2023-09-25 22:35:16 +0530

    Delete src directory

--- src/main/java/com/geekster/InstagramProject/repo/IAdminRepo.java
+++
@@ -1,7 +0,0 @@
-package com.geekster.InstagramProject.repo;
-
-import com.geekster.InstagramProject.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAdminRepo extends JpaRepository<Admin, Long> {
-}
--- src/main/java/com/geekster/InstagramProject/repo/IOtpTokenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.geekster.InstagramProject.repo;
-
-
-import com.geekster.InstagramProject.model.OtpAuthenticationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOtpTokenRepo extends JpaRepository<OtpAuthenticationToken, Long> {
-    OtpAuthenticationToken findByOtpToken(String token);
-}
